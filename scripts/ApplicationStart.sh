63b6df3bdcd3b91da24a38a82fdfa63425d910ad
Bhavesh-Gihar <bhaveshgihar@gmail.com>
2024-04-02 01:24:12 +0530

    First commit

---
+++ scripts/ApplicationStart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+##### Start Service app
+
+# cd /home/app/Client/
+# #####      Creating a service call client
+# sudo forever-service install client -r app
+# ##### start a service called client
+# sudo service client start
---
+++ scripts/BeforeInstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#update the system
+
+rm -rf /home/app/Client
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
