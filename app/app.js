30e82ba45c901fcb2b7d356f1fa34db1b86df5e8
Bhavesh-Gihar <bhaveshgihar@gmail.com>
2024-04-04 00:16:23 +0530

    Tenth Commit

--- app/app.js
+++ app/app.js
@@ -1,22 +1,17 @@
 const fs = require('fs');
 
-// Function to append text to a file
-function appendToFile() {
-    // Text to be appended
-    const text = "hi\n";
 
-    // Append text to the file
+function appendToFile() {
+    const text = "hi\n";
     fs.appendFile('example.txt', text, (err) => {
         if (err) throw err;
         console.log('Text appended to file');
     });
 }
 
-// Create a text file with initial text
 fs.writeFile('example.txt', '', (err) => {
     if (err) throw err;
     console.log('File created');
 
-    // Append text to the file every 10 seconds
     setInterval(appendToFile, 10000);
 });
